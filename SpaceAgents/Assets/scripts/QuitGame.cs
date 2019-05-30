662b566c7c9d2d72535535b6795973969e5aa8e5
Darlight <andres.perdomo09@gmail.com>
2019-05-29 23:24:12 -0600

    Main Menu controll support added

---
+++ SpaceAgents/Assets/scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        //Exits the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
