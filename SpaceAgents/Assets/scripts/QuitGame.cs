237ca4f6071440cefc9dcfdd0eca0976d66799fd
Darlight <andres.perdomo09@gmail.com>
2019-05-30 11:32:08 -0600

    ok

--- SpaceAgents/Assets/scripts/QuitGame.cs
+++ SpaceAgents/Assets/scripts/QuitGame.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class QuitGame : MonoBehaviour
 {
     public void Quit()
     {
         //Exits the game
-        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
